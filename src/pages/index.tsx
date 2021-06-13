ab926d308f33e5ba2cb4e88b231b4f3178a71832
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2021-06-14 02:56:53 +0900

    fix: :truck: githubpagesのパス的にルート指定だととれないので、NODE_ENVで切り替えられるか試す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,15 @@ import styled from 'styled-components'
 import ReactPlayer from 'react-player/lazy'
 
 export default function Home() {
+  const URL = (uniqueURL: string) => process.env.NODE_ENV === "production" ? `/hlsjs-playground${uniqueURL}` : uniqueURL
+
   return (<>
     <PlayerArea>
-      <ReactPlayer url='/data/video.m3u8' playing playsinline controls  muted width='100%'  config={{file: {
+      <ReactPlayer url={URL('data/video.m3u8')} playing playsinline controls  muted width='100%'  config={{file: {
         // forceHLS: true, // これiOSでもhls.js使っちゃう
       }}} />
       {/* 一応video直書きでhlsの動画を入れた場合も見たいので */}
-      <video src="/data/video.m3u8" controls muted></video>
+      <video src={URL('data/video.m3u8')} controls muted></video>
       <FuncButton>Button</FuncButton>
     </PlayerArea>
   </>)
